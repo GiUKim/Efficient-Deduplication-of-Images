3157977c352c65d549a45f202c0a4f31950d68fb
giukim <slasladk135@gmail.com>
2021-07-07 18:20:02 +0900

    [Biletral filter, weighted hash 적용]

--- config.py
+++ config.py
@@ -1,34 +1,44 @@
 
 class config(object):
     # 데이터셋의 이미지 총 개수
-    NUM_IMAGES = 21743
+    NUM_IMAGES = 22734
 
     # 동일 이미지로 판단 할 해밍 거리 기준값 (해밍거리가 낮을수록 동일한 이미지일 가능성 높음)
-    # 각 데이터셋 마다 기준이 달라질 수 있음
-    THRESHOLD = 101  # person
+    # 각 데이터셋 마다 기준이 달라짐
+
+    #THRESHOLD = 507  # person org465 weight(4)431 weight(6) 507
+    THRESHOLD = 400  # car org480 weight(2)400
+    #THRESHOLD = 510
+
+    # 32*32 이미지 에서 중앙의 8*8 만큼에 해당하는 hash 값에 대해서 weight 값 만큼 가중 연산
+    #CENTER_HASH_WEIGHT = 6  # person
+    CENTER_HASH_WEIGHT = 2  # car
+    # 32*32 이미지에서 해당 값에 해당하는 두께 만큼 4개의 가장자리에 대한 hash 연산을 제외
+    IGNORE_EDGE = 2  # person
 
     # dhash를 구하기 위해 원본 이미지를 리사이징할 때 리사이즈 크기
-    resize_width = 33 # 가로(dhash 연산때문에 2^n + 1로 하는걸 추천)
-    resize_height = 32 # 세로
+    # 가로
+    resize_width = 33
+    # 세로
+    resize_height = 32
 
     # 현재 이미지부터 몇 개의 이미지까지 비교할 것인지 (비교 데이터 수 제한)
-    MAX_ITERATE = 300  # person
+    #MAX_ITERATE = 500  # person
+    #MAX_ITERATE = 200 # person
+    MAX_ITERATE = 200 # car
 
-    # 몇 회의 filtering 작업을 진행 할 것인지
-    PROCESSING = 3  # person
-    # 각 filtering stage 마다 threshold의 감소량
-    TH_REDUCE = 3  # person
 
     # 원본 데이터셋이 존재하는 폴더 경로
-    DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\project1\\person'
-    # DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\project1\\image'
+    DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\pythonProject\\car'
+    #DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\pythonProject\\person'
 
     # 동일한 이미지를 제거한 새로운 데이터셋을 저장할 폴더 경로
-    NEW_DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\project1\\new_person'
+    SAVE_DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\pythonProject\\new_image_car'
+    #SAVE_DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\pythonProject\\person_new_image'
 
-    # 중복이라고 판단되어 제거된 이미지들이 저장된 폴더 경로
-    EXCEPT_DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\project1\\dup_image_person'
-    #EXCEPT_DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\project1\\dup_image'
+    # 중복으로 판단되어 제거된 이미지들이 저장된 폴더 경로로
+    EXCEPT_DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\pythonProject\\dup_image_car'
+    #EXCEPT_DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\pythonProject\\person_dup_image'
 
     # 아무것도 사용하지 않았을 때 예상 순환 횟수에 대해 효율성 계산
     def get_Efficiency(self, total_iter):
