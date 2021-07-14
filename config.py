468048360b9656bcf415f98f602daa176582a65e
giukim <slasladk135@gmail.com>
2021-07-14 13:48:20 +0900

    [AKAZE Featuer matching 방식으로 변경]

--- config.py
+++ config.py
@@ -1,53 +1,42 @@
 
-class config(object):
+class Config(object):
     # 데이터셋의 이미지 총 개수
-    NUM_IMAGES = 22734
+    NUM_IMAGES = 40000  # (수정 할 필요 없음)
+    CATEGORY = 'car'  # 데이터셋 폴더 이름 여기에 수정
 
-    # 동일 이미지로 판단 할 해밍 거리 기준값 (해밍거리가 낮을수록 동일한 이미지일 가능성 높음)
-    # 각 데이터셋 마다 기준이 달라짐
-
-    #THRESHOLD = 507  # person org465 weight(4)431 weight(6) 507
-    THRESHOLD = 400  # car org480 weight(2)400
-    #THRESHOLD = 510
-
-    # 32*32 이미지 에서 중앙의 8*8 만큼에 해당하는 hash 값에 대해서 weight 값 만큼 가중 연산
-    #CENTER_HASH_WEIGHT = 6  # person
-    CENTER_HASH_WEIGHT = 2  # car
-    # 32*32 이미지에서 해당 값에 해당하는 두께 만큼 4개의 가장자리에 대한 hash 연산을 제외
-    IGNORE_EDGE = 2  # person
-
-    # dhash를 구하기 위해 원본 이미지를 리사이징할 때 리사이즈 크기
-    # 가로
-    resize_width = 33
-    # 세로
-    resize_height = 32
+    # 각 데이터셋 마다 기준이 달라질 수 있음
+    THRESHOLD_RATE = 0.7  # car
+    THRESHOLD_SIM = 8  # car
+    # THRESHOLD_M = 0.7  # person
+    # THRESHOLD_C = 5  # person
 
     # 현재 이미지부터 몇 개의 이미지까지 비교할 것인지 (비교 데이터 수 제한)
-    #MAX_ITERATE = 500  # person
-    #MAX_ITERATE = 200 # person
-    MAX_ITERATE = 200 # car
-
+    #MAX_ITERATE = 1000  # person
+    MAX_ITERATE = 100  # car
 
     # 원본 데이터셋이 존재하는 폴더 경로
-    DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\pythonProject\\car'
-    #DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\pythonProject\\person'
+    DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\project1\\' + CATEGORY
+    # DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\project1\\image'
 
     # 동일한 이미지를 제거한 새로운 데이터셋을 저장할 폴더 경로
-    SAVE_DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\pythonProject\\new_image_car'
-    #SAVE_DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\pythonProject\\person_new_image'
+    NEW_DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\project1\\new_' + CATEGORY
 
-    # 중복으로 판단되어 제거된 이미지들이 저장된 폴더 경로로
-    EXCEPT_DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\pythonProject\\dup_image_car'
-    #EXCEPT_DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\pythonProject\\person_dup_image'
+    # 중복이라고 판단되어 제거된 이미지들이 저장된 폴더 경로
+    EXCEPT_DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\project1\\dup_' + CATEGORY
+    #EXCEPT_DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\project1\\dup_image'
+
+    DISCARD_DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\project1\\bad_' + CATEGORY
+
+    CLUSTER_DIRECTORY = 'C:\\Users\\AI\\PycharmProjects\\project1\\cluster_' + CATEGORY
 
     # 아무것도 사용하지 않았을 때 예상 순환 횟수에 대해 효율성 계산
     def get_Efficiency(self, total_iter):
         # 기존 시간복잡도는 O(n^2)
-        original_iter = config.NUM_IMAGES * config.NUM_IMAGES
+        original_iter = Config.NUM_IMAGES * Config.NUM_IMAGES
         return ((original_iter - total_iter) / original_iter) * 100.0
 
     # 비교 데이터 수에 제한을 걸었을 때 효율성 계산
     def get_Limited_Compare_Efficiency(self, total_iter):
         # 기존 시간복잡도는 O(n^k) k는 비교할 주변 이미지의 최대 개수
-        original_iter = config.NUM_IMAGES * config.MAX_ITERATE
+        original_iter = Config.NUM_IMAGES * Config.MAX_ITERATE
         return ((original_iter - total_iter) / original_iter) * 100.0
--- main.py
+++ main.py
@@ -2,18 +2,12 @@ import cv2
 import os
 import numpy as np
 import time
-from PIL import Image
-from config import config
-
-# import imagehash
-# import math
-# import matplotlib.pyplot as plt
-
+from config import Config
+from tqdm import tqdm
 
 def get_Hamming_Distance(org_image, tgt_image):
     difference = (int(org_image, 16)) ^ (int(tgt_image, 16))
     return bin(difference).count("1")
-
 
 def binary_Search(lst, find_number):
     low = 0
@@ -28,123 +22,124 @@ def binary_Search(lst, find_number):
             high = middle - 1
     return False
 
-
-def get_DHash_Value(hash_list):
-    # 반복횟수 22734 * 8 * 8 + 22734 * 64
-    for filename in os.listdir(config.DIRECTORY):
+# 모든 이미지의 Feature point(descriptor)를 구함
+def get_Feature_Point(cache):
+    print("\n\n[GET FEATURE POINT Working ... ]")
+    feature_list = []
+    discard_recommend_list = []
+    feature = cv2.AKAZE_create() # AKAZE 알고리즘으로 적용
+    image_count = 0
+    for filename in tqdm(os.listdir(config.DIRECTORY)):
         if filename.endswith(".jpg"):
-            # img = cv2.imread(os.path.join(config.DIRECTORY, filename), cv2.IMREAD_GRAYSCALE)
-            # img = cv2.GaussianBlur(img, (7, 7), 0)
-            # img = cv2.resize(img, dsize=(config.resize_width, config.resize_height), interpolation=cv2.INTER_LINEAR)
-            #
-            # t, t_otsu = cv2.threshold(img, -1, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-            # # print("threshold suggestion: {}".format(t))
-            # # t_otsu = t_otsu / 255
-            # # 32*32 otsu thresholding 된 이미지 보기
-            # # cv2.imshow("otsu", t_otsu)
-            # # cv2.waitKey(0)
-            # pixels = t_otsu.flatten().tolist()
-            # difference = []
-            # # 각 픽셀 차이 계산
-            # for row in range(config.resize_height):
-            #     row_start_index = row * config.resize_width
-            #     for col in range(config.resize_width - 1):
-            #         left_pixel_index = row_start_index + col
-            #         # # 이미지 4개의 가장자리(두께: 2)의 hash에 weight (x0)
-            #         # if (row <= 1) or (row >= 30) or (col <= 1) or (col >= 30):
-            #         #     continue
-            #         # # 이미지 중앙(8x8) 부분의 hash에 weight (x2)
-            #         # if (row >= 12 and row <= 19) and (col >= 12 and col <= 19):
-            #         #     difference.append(pixels[left_pixel_index] > pixels[left_pixel_index + 1])
-            #         difference.append(pixels[left_pixel_index] > pixels[left_pixel_index + 1])
-
             img = cv2.imread(os.path.join(config.DIRECTORY, filename), cv2.IMREAD_GRAYSCALE)
-            #img = cv2.bilateralFilter(img, 9, 75, 75)
-            #img = cv2.GaussianBlur(img, (config.GAUSSIAN_KERNEL, config.GAUSSIAN_KERNEL), 0)
-            img = cv2.resize(img, dsize=(config.resize_width, config.resize_height), interpolation=cv2.INTER_LINEAR)
-            # cv2.imshow("img", img)
-            # cv2.waitKey(0)
-            # img = Image.open(os.path.join(config.DIRECTORY, filename))
-            # img = img.resize((config.resize_width, config.resize_height))  # 9 * 8로 이미지 리사이징
-            # img = img.convert("L")  # gray s\cale로 차원 축소
-            img = img / 255.0
-            # cv2.imshow("img", img)
-            # cv2.waitKey(0)
-            pixels = img.flatten().tolist()
-            #pixels = list(img.getdata())
-            difference = []
-            # 각 픽셀 차이 계산
-            for row in range(config.resize_height):
-                row_start_index = row * config.resize_width
-                for col in range(config.resize_width - 1):
-                    left_pixel_index = row_start_index + col
-                    #이미지 4개의 가장자리(두께: 2)에 대해 hash 연산에 참여시키지 않음
-                    if (row <= config.IGNORE_EDGE - 1) or (row >= 32 - config.IGNORE_EDGE) or (col <= config.IGNORE_EDGE - 1) or (col >= 32 - config.IGNORE_EDGE):
-                        continue
-                    # 이미지 중앙(8x8) 부분의 hash에 weight (x4)
-                    if (row >= 12 and row <= 19) and (col >= 12 and col <= 19):
-                        for w in range(config.CENTER_HASH_WEIGHT - 1):
-                            difference.append(pixels[left_pixel_index] > pixels[left_pixel_index + 1])
-                    difference.append(pixels[left_pixel_index] > pixels[left_pixel_index + 1])
+            #img = cv2.GaussianBlur(img, (5, 5), 0)
+            kp, desc = feature.detectAndCompute(img, None)
+            # 아무 Feature 가 없는 이미지일 경우
+            if desc is None:
+                discard_recommend_list.append(image_count)  # 이미지 제거 제안을 위해 리스트에 삽입
+                cache[image_count] = True  # 연산 제외를 위해 cache masking
+            feature_list.append(desc)
+            image_count += 1
+    return feature_list, cache, discard_recommend_list
 
-            decimal_value = 0
-            hash_string = ""
-            # 16진수 dHash 생성
-            for index, value in enumerate(difference):
-                if value:
-                    #decimal_value += value * (2 ** (index % 8))
-                    decimal_value += (value << (index % 8))  # shift left 연산이 훨씬 빠름 (1초)
-                if index % 8 == 7:
-                    # rjust로 16진수가 두자리가 아닌 한자리가 나오면 MSB쪽을 0으로 채움
-                    hash_string += str(hex(decimal_value)[2:].rjust(2, "0"))
-                    decimal_value = 0
-            hash_list.append(hash_string)
+def find_Feature_Matching(feature_list, cache, remain_list, cluster):
+    print("\n\n[FIND MATCHING Working ... ]")
+    count_loop = 0  # 반복 횟수 계산용
 
+    for org_idx in tqdm(range(config.NUM_IMAGES - 1)):
+        sub_cluster = []
+        sub_cluster.append(org_idx)
 
-def get_Filtered_Images():
-    img_num = 0
-    remain_image_list = []
-    count_search = 0
-    count_cache = 0
-    # 최종 건져낼 이미지들을 새로운 리스트에 삽입
-    file_list = [filenames for (filenames) in os.listdir(config.DIRECTORY)]
-    file_list_jpg = [file for file in file_list if file.endswith(".jpg")]
-    for filename in file_list_jpg:
-        # 캐시에 마스킹 되어있지만 중복 집단 중 1개씩의 이미지를 삽입
-        if binary_Search(remain_list, img_num):
-            count_search += 1
-            remain_image_list.append(filename)
-        # 캐시에 마스킹 되어 있지 않은 순수한 이미지 삽입
-        elif not cache[img_num]:
-            count_cache += 1
-            remain_image_list.append(filename)
-        img_num += 1
+        # 비교 주체 이미지가 중복 판정이 되어있으면 skip
+        if cache[org_idx]:
+            sub_cluster.pop()
+            continue
+        # 비교 주체 이미지로부터 이후 연속 MAX_ITERATE 개의 이미지를 비교 대상 이미지로 선정
+        for tgt_idx in range(org_idx, org_idx + config.MAX_ITERATE):
+            matcher = cv2.BFMatcher_create(cv2.NORM_HAMMING)
+            if tgt_idx > config.NUM_IMAGES - 1:
+                break
+            if org_idx == tgt_idx or cache[tgt_idx]:
+                continue
+            count_loop += 1
+            matches = matcher.knnMatch(feature_list[org_idx], feature_list[tgt_idx], 2)
+            similarity = 0
+            is_match = False
+            for m in matches:
+                if len(m) < 2:
+                    continue
+                if m[0].distance / m[1].distance < config.THRESHOLD_RATE:
+                    similarity += 1
+                    if similarity >= config.THRESHOLD_SIM:
+                        is_match = True
+                        break
+            if is_match:
+                sub_cluster.append(tgt_idx)
+                cache[tgt_idx] = True
+                remain_list.append(org_idx)
+                cache[org_idx] = True
+        cluster.append(sub_cluster)
+    return count_loop, remain_list, cluster
 
-    return remain_image_list, count_search, count_cache
+def save_Result_At_Directory(cache, remain_list, discard_recommend_list, cluster):
 
+    # 중복 제거 된 이미지 저장 할 디렉토리 생성(이미 존재하면 모든 파일 제거)
+    if os.path.isdir(config.NEW_DIRECTORY):
+        for file in os.scandir(config.NEW_DIRECTORY):
+            os.remove(file.path)
+    elif not os.path.isdir(config.NEW_DIRECTORY):
+        os.mkdir(config.NEW_DIRECTORY)
 
-def save_Result_At_Directory(remain_image_list, cache, remain_list):
-    # 필터링 된 이미지 저장 할 디렉토리 생성
-    if not os.path.isdir(config.SAVE_DIRECTORY):
-        os.mkdir(config.SAVE_DIRECTORY)
-
-    # 제외 된 이미지 저장 할 디렉토리 생성
-    if not os.path.isdir(config.EXCEPT_DIRECTORY):
+    # 제외 된 이미지 저장 할 디렉토리 생성(이미 존재하면 모든 파일 제거)
+    if os.path.isdir(config.EXCEPT_DIRECTORY):
+        for file in os.scandir(config.EXCEPT_DIRECTORY):
+            os.remove(file.path)
+    elif not os.path.isdir(config.EXCEPT_DIRECTORY):
         os.mkdir(config.EXCEPT_DIRECTORY)
 
-    # 필터링 되고 남은 이미지 저장
-    for filename in remain_image_list:
-        org_img = cv2.imread(os.path.join(config.DIRECTORY, filename), cv2.IMREAD_COLOR)
-        cv2.imwrite(os.path.join(config.SAVE_DIRECTORY, filename), org_img)
+    # 삭제를 추천하는 이미지 저장 할 디렉토리 생성(이미 존재하면 모든 파일 제거)
+    # Feature가 1개도 발견되지 못한 이미지들
+    # 심한 Blur, Noise 등등
+    if os.path.isdir(config.DISCARD_DIRECTORY):
+        for file in os.scandir(config.DISCARD_DIRECTORY):
+            os.remove(file.path)
+    elif not os.path.isdir(config.DISCARD_DIRECTORY):
+        os.mkdir(config.DISCARD_DIRECTORY)
 
-    # 중복으로 판단되어 제외 된 이미지도 따로 저장
+    # 클러스터링된 이미지들을 구별하여 저장 할 디렉토리 생성(이미 존재하면 모든 파일 제거)
+    if os.path.isdir(config.CLUSTER_DIRECTORY):
+        for file in os.scandir(config.CLUSTER_DIRECTORY):
+            os.remove(file.path)
+    elif not os.path.isdir(config.CLUSTER_DIRECTORY):
+        os.mkdir(config.CLUSTER_DIRECTORY)
+
     image_num = 0
-    for filename in os.listdir(config.DIRECTORY):
+    print("\n\n[SAVE TO DIRECTORY Working ... ]")
+    for filename in tqdm(os.listdir(config.DIRECTORY)):
         if filename.endswith(".jpg"):
-            if (cache[image_num]) and (not binary_Search(remain_list, image_num)):
-                img = cv2.imread(os.path.join(config.DIRECTORY, filename), cv2.IMREAD_COLOR)
+            img = cv2.imread(os.path.join(config.DIRECTORY, filename), cv2.IMREAD_COLOR)
+            # 살아남은 이미지들 NEW_DIRECTORY 에 저장
+            if (not cache[image_num] or binary_Search(remain_list, image_num)) \
+                    and not binary_Search(discard_recommend_list, image_num):
+                cv2.imwrite(os.path.join(config.NEW_DIRECTORY, filename), img)
+            # 살아남은 이미지들 NEW_DIRECTORY 에 저장
+            elif binary_Search(discard_recommend_list, image_num):
+                cv2.imwrite(os.path.join(config.DISCARD_DIRECTORY, filename), img)
+            else:
                 cv2.imwrite(os.path.join(config.EXCEPT_DIRECTORY, filename), img)
             image_num += 1
+    print("\n\n[CREATE CLUSTER OF IMAGE Working ... ]")
+    cluster_num = 0
+    for sub_cluster in tqdm(cluster):
+        cluster_num += 1
+        if len(sub_cluster) > 1:
+            idx = 0
+            for img_num in sub_cluster:
+                idx += 1
+                filename = os.listdir(config.DIRECTORY)[img_num]
+                if filename.endswith(".jpg"):
+                    img = cv2.imread(os.path.join(config.DIRECTORY, filename), cv2.IMREAD_COLOR)
+                    cv2.imwrite(os.path.join(config.CLUSTER_DIRECTORY, str(cluster_num) + "_" + str(idx) + ".jpg"), img)
 
 def check_image_size() :
     count = 0
@@ -159,60 +154,38 @@ def check_image_size() :
             count += 1
     return count
 
-if __name__ == '__main__':
-    config = config()
-    print("BEFORE config.NUM_IMAGES : " , config.NUM_IMAGES)
-    config.NUM_IMAGES = check_image_size()
-    print("AFTER config.NUM_IMAGES : " , config.NUM_IMAGES)
-
-    cache = np.zeros(config.NUM_IMAGES, dtype=bool)  # 이미지 개수만큼 bool type의 캐시 메모리 생성
-    hash_list = []
-    start = time.time()  # 타이머 시작
-    #cv2.ocl.setUseOpenCL(True)  # 속도 올려준다고 하는데 잘 모르겠음
-    # 모든 이미지에 대하여 dhash 값 구하기
-    get_DHash_Value(hash_list)
-
-    count_loop = 0
-    remain_list = []
-    # 전체 이미지 순회하며 비교 (time-dominant loop)
-    #print(get_Hamming_Distance(hash_list[0], hash_list[1]))
-    for org_idx in range(config.NUM_IMAGES - 1):
-        if cache[org_idx]:
-            continue
-        else:
-            for tgt_idx in range(org_idx, org_idx + config.MAX_ITERATE):
-                if tgt_idx > config.NUM_IMAGES - 1:
-                    break
-                if org_idx == tgt_idx: #or cache[tgt_idx]:
-                    continue
-
-                count_loop += 1
-                similarity = get_Hamming_Distance(hash_list[org_idx], hash_list[tgt_idx])
-                if similarity < config.THRESHOLD:
-                    # if cache[org_idx] and not cache[tgt_idx]:
-                    #     cache[tgt_idx] = True
-                    # else:
-                    cache[org_idx] = True   # 비교한 이미지의 인덱스에 해당하는 캐시 메모리에 마스킹
-                    remain_list.append(org_idx)    # 비교한 이미지는 살려야 하므로 따로 리스트에 저장
-                    cache[tgt_idx] = True   # 비교된 이미지의 인덱스에 해당하는 캐시 메모리에 마스킹
-                #print(similarity)
-
-    remain_image_list = []
-    count_search = 0
-    count_cache = 0
-    remain_image_list, count_search, count_cache = get_Filtered_Images()
-
-    elapsed_time = time.time() - start
+def Summary(count_loop, elapsed_time):
+    # 각 효율성 계산
     efficiency = config.get_Efficiency(total_iter=count_loop)
     limited_efficiency = config.get_Limited_Compare_Efficiency(total_iter=count_loop)
 
     # spec summary
     print("주 소요구간 반복 횟수: {}".format(count_loop))
     print("소요 시간: {} sec".format(round(elapsed_time, 4)))
-    print("중복 그룹 개수, 미중복 이미지 개수: {} {}".format(count_search, count_cache))
-    print("필터링 후 남은 이미지 개수: {}".format(len(remain_image_list)))
     print("(비교 데이터 수 제한)시간 효율: {}%".format(round(limited_efficiency, 4)))
     print("(총)시간 효율: {}%".format(round(efficiency, 4)))
 
-    save_Result_At_Directory(remain_image_list, cache, remain_list)
 
+if __name__ == '__main__':
+    config = Config()
+    print("BEFORE config.NUM_IMAGES : " , config.NUM_IMAGES)
+    config.NUM_IMAGES = check_image_size()
+    print("AFTER config.NUM_IMAGES : " , config.NUM_IMAGES)
+    # 이미지 개수만큼 boolean type 의 캐시 메모리 생성
+    cache = np.zeros(config.NUM_IMAGES, dtype=bool)
+    start = time.time()  # 타이머 시작
+
+    #cv2.ocl.setUseOpenCL(True)  # 속도 올려준다고 하는데 잘 모르겠음
+    remain_list = []
+    cluster = []
+    # 모든 이미지들에 대해서 Feature descriptor 구하기
+    feature_list, cache, discard_recommend_list = get_Feature_Point(cache)
+    # 이미지들끼리 Feature Matching을 통해 비교
+    count_loop, remain_list, cluster = find_Feature_Matching(feature_list, cache, remain_list, cluster)
+
+    elapsed_time = time.time() - start # 타이머 종료
+
+    # 결과 요약
+    Summary(count_loop, elapsed_time)
+    # 이미지 저장
+    save_Result_At_Directory(cache, remain_list, discard_recommend_list, cluster)
